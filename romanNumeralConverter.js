3fca126f9398803790474e6a12c148a90be57848
Jennifer Phelps <albi0025@gmail.com>
2017-02-20 16:28:19 -0700

    We did it

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -1,4 +1,47 @@
-function romanNumberalConverter(roman) {
+// have a function that takes a roman numeral and asigns a number value and returns that value.
+  var romanMap = {
+    "I":1,
+    "V":5,
+    "X":10,
+    "L":50,
+    "C":100,
+    "D":500
+  }
+function romanCompare(lastChar, romanDigit) {
+  return lastChar && romanMap[lastChar] > romanMap[romanDigit];
+}
+function convertRound(lastChar, total, romanDigit, valRomanDigit) {
+  if (romanCompare(lastChar, romanDigit)) {
+    total -=valRomanDigit;
+  } else {
+    total +=valRomanDigit;
+  }
+  return total;
 }
 
-module.exports = romanNumberalConverter;
+
+function romanNumeralConverter(roman) {
+  var lastChar = "";
+  var total = 0;
+  for(var i = roman.length; i >= 0; i--){
+    if(roman[i] === "V") {
+      total +=5;
+    } else if(roman[i] === "I") {
+       total = convertRound(lastChar, total, "I", 1);
+    } else if(roman[i] === "X") {
+       total = convertRound(lastChar, total, "X", 10);
+    } else if(roman[i] === "L") {
+       total +=50;
+    } else if(roman[i] === "C") {
+       total = convertRound(lastChar, total, "C", 100);
+    } else if(roman[i] === "D") {
+       total +=500;
+    }
+    lastChar = roman[i];
+  }
+  return total;
+}
+
+
+
+module.exports = romanNumeralConverter;
--- romanNumeralConverterTest.js
+++ romanNumeralConverterTest.js
@@ -1,22 +1,81 @@
 var assert = require('chai').assert;
 var romanNumeralConverter = require('./romanNumeralConverter');
 
+function conversionTest(number, numeral) {
+  //Arrange
+  var expected = number;
+  //Act
+  var actual = romanNumeralConverter(numeral);
+  //Assert
+  assert.equal(actual, expected);
+}
+
+
 describe("RomanNumeralConverter", function() {
   it("can convert I to 1", function() {
-    //Arrange
-    var expected = 1;
-    //Act
-    var actual = romanNumeralConverter("I");
-    //Assert
-    assert.equal(actual, expected);
+    conversionTest(1, "I");
   });
-
-  xit("can convert V to 5", function() {
-    //Arrange
-    var expected = 5;
-    //Act
-    var actual = romanNumeralConverter("V");
-    //Assert
-    assert.equal(actual, expected);
+  it("can convert II to 2", function() {
+    conversionTest(2, "II");
+  });
+  it("can convert III to 3", function() {
+    conversionTest(3, "III");
+  });
+  it("can convert IV to 4", function() {
+    conversionTest(4, "IV");
+  });
+  it("can convert V to 5", function() {
+    conversionTest(5, "V");
+  });
+  it("can convert VI to 6", function() {
+    conversionTest(6, "VI");
+  });
+  it("can convert VII to 7", function() {
+    conversionTest(7, "VII");
+  });
+  it("can convert VIII to 8", function() {
+    conversionTest(8, "VIII");
+  });
+  it("can convert IX to 9", function() {
+    conversionTest(9, "IX");
+  });
+  it("can convert X to 10", function() {
+    conversionTest(10, "X");
+  });
+  it("can convert XI to 11", function() {
+    conversionTest(11, "XI");
+  });
+  it("can convert XII to 12", function() {
+    conversionTest(12, "XII");
+  });
+  it("can convert XIII to 13", function() {
+    conversionTest(13, "XIII");
+  });
+  it("can convert XIV to 14", function() {
+    conversionTest(14, "XIV");
+  });
+  it("can convert XX to 20", function() {
+    conversionTest(20, "XX");
+  });
+  it("can convert XL to 40", function() {
+    conversionTest(40, "XL");
+  });
+  it("can convert L to 50", function() {
+    conversionTest(50, "L");
+  });
+  it("can convert LXXXIX to 89", function() {
+    conversionTest(89, "LXXXIX");
+  });
+  it("can convert XC to 90", function() {
+    conversionTest(90, "XC");
+  });
+  it("can convert C to 100", function() {
+    conversionTest(100, "C");
+  });
+  it("can convert D to 500", function() {
+    conversionTest(500, "D");
+  });
+  it("can convert CD to 400", function() {
+    conversionTest(400, "CD");
   });
 });
